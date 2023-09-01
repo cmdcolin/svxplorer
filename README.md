5fc416478a7311aaf53c2722eb7e5d6c48d0d92c
Colin <colin.diesh@gmail.com>
2023-09-01 11:42:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# sv_browser_hackathon
+
+Creating a browser for parliament2 and other SV callers on the HG002 dataset,
+with links to JBrowse 2
---
+++ src/App.tsx
@@ -0,0 +1,226 @@
+import { useEffect, useState } from 'react'
+import VCF from '@gmod/vcf'
+import './App.css'
+
+const files = [
+  'calls/HG002.2X250.downsampled.50X.breakseq.vcf',
+  'calls/HG002.2X250.downsampled.50X.cnvnator.svtyped.vcf',
+  'calls/HG002.2X250.downsampled.50X.cnvnator.vcf',
+  'calls/HG002.2X250.downsampled.50X.combined.genotyped.vcf',
+  'calls/HG002.2X250.downsampled.50X.delly.deletion.vcf',
+  'calls/HG002.2X250.downsampled.50X.delly.duplication.vcf',
+  'calls/HG002.2X250.downsampled.50X.delly.insertion.vcf',
+  'calls/HG002.2X250.downsampled.50X.delly.svtyped.vcf',
+  'calls/HG002.2X250.downsampled.50X.lumpy.svtyped.vcf',
+  'calls/HG002.2X250.downsampled.50X.lumpy.vcf',
+  'calls/HG002.2X250.downsampled.50X.manta.svtyped.vcf',
+  'calls/HG002.2X250.downsampled.50X.breakdancer.svtyped.vcf',
+  'calls/HG002.2X250.downsampled.50X.breakdancer.vcf',
+  'calls/HG002.2X250.downsampled.50X.breakseq.svtyped.vcf',
+]
+function App() {
+  const [value, setValue] = useState(files[0])
+  const [mode, setMode] = useState('parsed')
+  return (
+    <>
+      <h1>Team 7: Mapping vs assembly based SV calls</h1>
+      <label htmlFor="file_box">Select a file to browse</label>
+      <select
+        id="file_box"
+        value={value}
+        onChange={event => setValue(event.target.value)}
+      >
+        {files.map(f => (
+          <option key={f} value={f}>
+            {f}
+          </option>
+        ))}
+      </select>
+      <fieldset>
+        <div>
+          <input
+            type="radio"
+            id="raw"
+            name="mode"
+            value="raw"
+            checked={mode === 'raw'}
+            onChange={event => setMode(event.target.value)}
+          />
+          <label htmlFor="raw">Raw VCF</label>
+        </div>
+
+        <div>
+          <input
+            type="radio"
+            id="parsed"
+            name="mode"
+            value="parsed"
+            checked={mode === 'parsed'}
+            onChange={event => setMode(event.target.value)}
+          />
+          <label htmlFor="parsed">Parsed VCF</label>
+        </div>
+      </fieldset>
+
+      <Table mode={mode} filename={value} />
+    </>
+  )
+}
+
+function Table({ mode, filename }: { mode: string; filename: string }) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [data, setData] = useState<any[]>()
+  const [error, setError] = useState<unknown>()
+  useEffect(() => {
+    // eslint-disable-next-line no-extra-semi
+    ;(async () => {
+      try {
+        const response = await fetch(filename)
+        if (!response.ok) {
+          throw new Error(
+            `HTTP ${
+              response.status
+            } fetching ${filename} ${await response.text()}`,
+          )
+        }
+        const data = await response.text()
+        const parser = new VCF({
+          header: data
+            .split('\n')
+            .filter(f => f.startsWith('#'))
+            .join('\n'),
+        })
+        const lines = data
+          .split('\n')
+          .filter(f => !f.startsWith('#'))
+          .filter(f => !!f)
+          .map(line => ({
+            parsed: parser.parseLine(line),
+            line: line.split('\t'),
+          }))
+        setData(lines)
+      } catch (e) {
+        setError(e)
+        console.error(e)
+      }
+    })()
+  }, [filename])
+  const rows = data
+  return error ? (
+    <div style={{ color: 'red' }}>{`${error}`}</div>
+  ) : (
+    <>
+      {mode === 'raw' ? (
+        <RawVCF key={filename} rows={rows} />
+      ) : (
+        <ParsedVCF key={filename} rows={rows} />
+      )}
+    </>
+  )
+}
+
+function RawVCF({ rows }: { rows?: any[] }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>CHROM</th>
+          <th>START</th>
+          <th>ID</th>
+          <th>REF</th>
+          <th>ALT</th>
+          <th>QUAL</th>
+          <th>FILTER</th>
+          <th>INFO</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows?.map(({ line }) => (
+          <tr key={line.join('\t')}>
+            <td>{line[0]}</td>
+            <td>{(+line[1]).toLocaleString('en-US')}</td>
+            <td>{line[2]}</td>
+            <td>{line[3]}</td>
+            <td>{line[4]}</td>
+            <td>{line[5]}</td>
+            <td>{line[6]}</td>
+            <td>{line[7]}</td>
+            <td>Hello</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
+
+function shorten(val: string) {
+  return val.length > 100 ? val.slice(0, 100) + '...' : val
+}
+
+function ParsedVCF({
+  rows = [],
+}: {
+  rows?: { line: string[]; parsed: any }[]
+}) {
+  const keys = new Set<string>()
+  for (const row of rows.slice(0, 100)) {
+    for (const k of Object.keys(row.parsed.INFO)) {
+      keys.add(k)
+    }
+  }
+  keys.delete('END')
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>JB2 LINK</th>
+          <th>LENGTH</th>
+          <th>ID</th>
+          <th>REF</th>
+          <th>ALT</th>
+          <th>QUAL</th>
+          <th>FILTER</th>
+          {[...keys].map(k => (
+            <th key={k}>{k}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {rows.slice(0, 100).map(({ parsed, line }) => {
+          const start = +line[1]
+          const end = +(parsed.INFO?.END?.[0] || 0)
+          const len = (end - start).toLocaleString('en-US')
+          const s = start.toLocaleString('en-US')
+          const e = start.toLocaleString('en-US')
+          return (
+            <tr key={line.join('\t')}>
+              <td>
+                <a
+                  href={`https://jbrowse.org/code/jb2/v2.6.3/?config=/demos/hg002_demo/config.json&assembly=hg19&loc=${line[0]}:${start}-${end}`}
+                  target="_blank"
+                >
+                  {`${line[0]}:${s}-${e}`}
+                </a>
+              </td>
+              <td>{len}</td>
+              {line.slice(2, 7).map(elt => (
+                <td key={elt}>{shorten(elt)}</td>
+              ))}
+              {[...keys].map(k => {
+                const val = parsed.INFO[k] || ''
+                return (
+                  <td key={k}>
+                    {val.length > 100 ? val.slice(0, 100) + '...' : val}
+                  </td>
+                )
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
+
+export default App
